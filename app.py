0512a615a417d022d770fa11a32ac4b1cc1fcf40
gazaboo <florian.dadouchi@gmail.com>
2025-03-24 19:49:55 +0100

    firtst test db

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@127.0.0.1:5432/{os.getenv('DB_NAME')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello Monde!</p>"
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    data = request.json
+    new_user = User(username=data['username'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully'}), 201
