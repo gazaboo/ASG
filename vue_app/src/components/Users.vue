a8bc4377ce242032c635587270a517750164df7c
gazaboo <florian.dadouchi@gmail.com>
2025-03-26 13:46:24 +0100

    trying to fetch data from vue app

---
+++ vue_app/src/components/Users.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Users</h1>
+        <ul>
+            <li v-for="user in users" :key="user.id">
+                {{ user.username }} ({{ user.email }})
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: []
+        };
+    },
+    created() {
+        this.fetchUsers();
+    },
+    methods: {
+        async fetchUsers() {
+            try {
+                const response = await fetch('/users');
+                const data = await response.json();
+                this.users = data;
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+/* Add any styles you need here */
+</style>
