913b97cb4ee71e3aeb12e16f5aecc86292a30759
gazaboo <florian.dadouchi@gmail.com>
2025-03-27 15:40:59 +0100

    adding local and deploy config

--- vue_app/vite.config.js
+++ vue_app/vite.config.js
@@ -3,8 +3,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const basePath = '/'
-// const basePath = '/test_python/'
+// const basePath = '/'
+const basePath = '/test_python/'
 
 export default defineConfig({
   base: basePath,
